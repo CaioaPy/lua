83275fb19ad42f5eeaa0d45c6f7082c83b1d8dac
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-10-31 10:47:40 -0300

    sum function

--- firstlua.lua
+++ firstlua.lua
@@ -14,3 +14,29 @@ for i = 1,100
       print(i)
       end
 ]]
+tablew = {}
+tablew["a"] = 10
+x = tablew["a"]
+y = tablew.a --another notation
+print(x)
+print(y)
+
+a = {}
+for i=1,10 do
+    a[i] = io.read()
+end
+print(a[10])
+
+--sum test
+a = {}
+for i=1,10 do
+    a[i] = io.read("*n")
+end
+function add (a)
+      local sum = 0
+      for i,v in ipairs(a) do
+        sum = sum + v
+      end
+      return sum
+end
+print(add(a))
