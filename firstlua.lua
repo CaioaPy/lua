54ad114638c551b8dba00f062f7f11bc4baf9be9
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-10-31 10:28:04 -0300

    basic for loop

--- firstlua.lua
+++ firstlua.lua
@@ -3,3 +3,14 @@ function conc (va)
       return f
 end
 x = conc("ew") --output = function but x stays nil
+--simple for loop struture
+for i = 1,100
+      do
+      print(i)
+      end
+--[[
+for i = 1,100
+      do
+      print(i)
+      end
+]]
