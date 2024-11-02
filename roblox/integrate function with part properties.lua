d176c68b18e99e851e4c4aabca3686336504614c
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-11-02 20:32:32 -0300

    Update integrate function with part properties.lua

--- roblox/integrate function with part properties.lua
+++ roblox/integrate function with part properties.lua
@@ -1,1 +1,23 @@
+local UserInputService = game:GetService("UserInputService")
 
+part = workspace.MyPart
+
+local function RandomColor(part)
+		part.BrickColor = BrickColor.Random()
+end
+
+local function RandomOpacity(part)
+		part.Transparency.Random()
+end
+
+if Enum.KeyCode.T then
+	RandomOpacity(part)
+end
+
+if Enum.KeyCode.R then
+	RandomColor(part)
+end
+
+UserInputService.InputBegan:Connect(RandomColor)
+
+UserInputService.InputBegan:Connect(RandomOpacity)
