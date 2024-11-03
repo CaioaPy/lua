44fa9185310e53b69c395f1aeb72d6f4d8f80aaf
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-11-03 09:06:04 -0300

    Update kill function with timer.lua

--- roblox/kill function with timer.lua
+++ roblox/kill function with timer.lua
@@ -1,1 +1,15 @@
+local UserInputService = game:GetService("UserInputService")
 
+function KillAll()
+	for _, player in pairs(game.Players:GetChildren()) do
+		local player = player.character
+		player:waitForChild("Humanoid").Health = 0
+	end	
+end
+
+UserInputService.InputBegan:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.L then
+		wait(5)
+		KillAll()
+	end
+end)
