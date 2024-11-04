206ef3b2631f6c967489397ab860632fdd192162
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-11-04 16:03:49 -0300

    Update Party Server.lua

--- roblox/Local to server/Party Server.lua
+++ roblox/Local to server/Party Server.lua
@@ -1,1 +1,21 @@
+local part = game.Workspace.PartyPart
+
+local function RandomColor(part)
+	local r = math.random(0, 255)
+	local g = math.random(0, 255)
+	local b = math.random(0, 255)
+	part.Color = Color3.fromRGB(r, g, b)
+end
+
+local function loopfunc(quantity)
+	for i = 1, quantity do
+		RandomColor(part)
+		task.wait(1)
+	end
+end
+
+game.ReplicatedStorage.PartyTrigger.OnServerEvent:Connect(function(player, number)
+	print(player)
+	loopfunc(number)
+end)
 
